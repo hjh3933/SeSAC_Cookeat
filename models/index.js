6911192a80ecef960f089b3b0b7f9eccf29d05e4
yhs0329 <scar0329@naver.com>
2024-02-28 15:36:51 +0900

    feat:모델 생성 및 추가

--- models/index.js
+++ models/index.js
@@ -4,12 +4,47 @@ const Sequelize = require("sequelize");
 const config = require(__dirname + "/../config/config.json")["development"];
 const db = {};
 
-const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+// (2) 모델 불러오기, sequelize 인스턴스와 Sequelize 모듈을 전달
+const Bookmarks = require("./Bookmarks")(sequelize, Sequelize);
+const Follows = require("./Follows")(sequelize, Sequelize);
+const Posts = require("./Posts")(sequelize, Sequelize);
+const Users = require("./Users")(sequelize, Sequelize);
+
+Users.hasMany(Posts, {
+    foreignKey: "id",
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+});
+Posts.belongsTo(Users, {
+    foreignKey: "id",
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+});
+
+Users.belongsToMany(Posts, {
+    through: Bookmarks,
+    foreignKey: "id",
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+});
+Posts.belongsToMany(Users, {
+    through: Bookmarks,
+    foreignKey: "postId",
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+});
+Users.belongsToMany(Users, {
+    foreignKey: "followingId",
+    as: "Followers",
+    through: Follows,
+});
+Users.belongsToMany(Users, {
+    foreignKey: "followerId",
+    as: "Followings",
+    through: Follows,
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
